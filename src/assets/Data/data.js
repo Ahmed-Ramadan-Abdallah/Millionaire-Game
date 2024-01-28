f799a7d7c185cb245c592fae5bee050373ea2fa0
Ahmed-Ramadan-8513 <ahmedramadan8513@gmail.com>
2024-01-28 20:21:43 +0200

    Upload Files

---
+++ src/assets/Data/data.js
@@ -0,0 +1,80 @@
+const  quizQuestions = [
+  {
+    question: "What is the capital of France ?",
+    answers: ["Madrid", "Berlin", "Rome", "Paris"],
+    correctAnswer: "Paris"
+  },
+  {
+    question: "What is the largest planet in the solar system ?",
+    answers: ["Earth", "Mars","Jupiter","Venus"],
+    correctAnswer: "Jupiter"
+  },
+  {
+    question: "What gas do plants absorb from the atmosphere ?",
+    answers: ["Carbon Dioxide", "Oxygen","Nitrogen","Hydrogen"],
+    correctAnswer: "Carbon Dioxide"
+  },
+  {
+    question: "What animal is known as the desert neighbor ?",
+    answers: ["camel", "leopard", "kangaroo", "crocodile"],
+    correctAnswer: "camel"
+  },
+  {
+    question: "How many planets are in the solar system?",
+    answers: ["8", "9", "10", "7"],
+    correctAnswer: "8"
+  },
+  {
+    question: "What is the first element on the periodic table?",
+    answers:["Hydrogen", "Oxygen", "Carbon", "Nitrogen"],
+    correctAnswer: "Hydrogen"
+  },
+  {
+    question: "What is the largest continent in the world?",
+    answers: ["Africa", "North America", "Asia", "Europe"],
+    correctAnswer: "Asia"
+  },
+  {
+    question: "How many days a week?",
+    answers: ["7", "5", "6", "8"],
+    correctAnswer: "7"
+  },
+  {
+    question: "What is the largest ocean in the world?",
+    answers: ["Pacific", "Atlantic", "Indian", "Arctic"],
+    correctAnswer: "Pacific"
+  },
+  {
+  question: "What is the primary color that indicates danger in traffic lights?",
+  answers: ["green", "red", "yellow", "blue"],
+  correctAnswer: "red"
+},
+{
+  question: "What is the capital of Egypt?",
+  answers: ["Cairo", "Alexandria", "Giza", "Luxor"],
+  correctAnswer: "Cairo"
+},
+{
+  question: "What is the longest river in the world?",
+  answers: ["Nile", "Amazon", "Mississippi", "Yangtze"],
+  correctAnswer: "Nile"
+},
+  {
+    question: "What is the capital of Turkey?",
+    answers: ["Ankara", "Istanbul", "Antalya", "Bursa"],
+    correctAnswer: "Ankara"
+  },
+  {
+    Question: "What is the capital of the United States of America?",
+    answers: ["Washington", "New York", "Los Angeles", "Chicago"],
+    correctAnswer: "Washington"
+  },
+  {
+    question: "Who is the author of the novel ' One Thousand and One Nights'?",
+    answers: ["Abu Al-Qasim Al-Shabi", "Al-Jahiz", "Al-Hasan Al-Basri", "Al-Khalil bin Ahmed"],
+    correctAnswer: "Al-Khalil bin Ahmed"
+  }
+];
+
+export default  quizQuestions
+
